db4c391030dcd3fc31a46e0e59929833bd116449
Shaik-Abdul-Ahad <79002637+Shaik-Abdul-Ahad@users.noreply.github.com>
2021-06-10 11:38:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+canvas = document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+car1_width=120;
+car1_height=70;
+car1_image="car1.jpg"
+car1_x=10;
+car1_y=10;
+
+
+car2_width=120;
+car2_height=70;
+car2_image="car2.jpg"
+car2_x=10;
+car2_y=100;
+
+
+background_image="car_background.jpg";
+
+function add() {
+    background_imgTag=new Image();
+    background_imgTag.onload=uploadBackground;
+    background_imgTag.src=background_image;
+    
+
+    car1_imgTag=new Image();
+    car1_imgTag.onload=uploadcar1;
+    car1_imgTag.src=car1_image;
+
+    
+    car2_imgTag=new Image();
+    car2_imgTag.onload=uploadcar2;
+    car2_imgTag.src=car2_image;
+}
+
+function uploadBackground() {
+    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+}
+
+
+function uploadcar1() {
+    ctx.drawImage(car1_imgTag,car1_x,car1_y,car1_width,car1_height);
+}
+
+
+function uploadcar2() {
+    ctx.drawImage(car2_imgTag,car2_x,car2_y,car2_width,car2_height);
+}
+
+
+window.addEventListener("keydown" , my_keydown);
+
+function my_keydown(e) {
+    keyPressed=e.keyCode;
+    if(keyPressed=='38') {
+        up();
+    }
+
+    if(keyPressed=='40') {
+        down();
+    }
+
+    if(keyPressed=='37') {
+        left();
+    }
+
+    if(keyPressed=='39') {
+        right();
+    }
+       
+    
+}
